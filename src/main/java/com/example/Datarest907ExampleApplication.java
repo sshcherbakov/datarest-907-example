ce6d7ea7cbc5d2bf7e85fa258d89791626d3f969
Sergey Shcherbakov <sergey.shcherbakov@qiotec.com>
2016-09-28 15:13:06 +0200

    Add unit test illustrating the problem

--- src/main/java/com/example/Datarest907ExampleApplication.java
+++ src/main/java/com/example/Datarest907ExampleApplication.java
@@ -1,14 +1,33 @@
 package com.example;
 
+import javax.annotation.PostConstruct;
+
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import com.example.entity.MyEntity;
+import com.example.repository.MyEntityRepository;
+
 @SpringBootApplication
-@EnableMongoRepositories
+@EnableMongoRepositories	// <-- This explicit one is causing the DATAREST-907 issue
 public class Datarest907ExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Datarest907ExampleApplication.class, args);
 	}
+	
+	@Autowired MyEntityRepository repository;
+
+	@PostConstruct
+	public void init() {
+
+		MyEntity entity = new MyEntity();
+		entity.setMyJodaTime(new DateTime());
+
+		repository.save(entity);
+	}
+	
 }
--- src/test/java/com/example/Datarest907ExampleApplicationTests.java
+++ src/test/java/com/example/Datarest907ExampleApplicationTests.java
@@ -1,11 +1,43 @@
 package com.example;
 
-import org.junit.Test;
+import static com.jayway.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.jayway.restassured.RestAssured;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Datarest907ExampleApplication.class, webEnvironment=WebEnvironment.RANDOM_PORT)
 public class Datarest907ExampleApplicationTests {
 
+	@LocalServerPort
+    private int port;
+
+	@Before
+	public void setUp() {
+		RestAssured.baseURI = "http://localhost:"+port;
+	}
+	
 	@Test
-	public void contextLoads() {
+	public void testDatarest907() {
+		
+        given().
+		when().
+		        get("/myentities").
+		then().
+		        statusCode(SC_OK).
+				body("_embedded.myEntities.myJodaTime", everyItem(is(instanceOf(String.class))));
+
 	}
 
 }
